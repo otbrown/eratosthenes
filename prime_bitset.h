bd8e5f06a35aa69d31e8e8f53653e72b9f80b3f6
Oliver Thomson Brown <otbrown@users.noreply.github.com>
2021-03-01 18:11:32 +0000

    prime_bitset.h: bitset for B=100

---
+++ prime_bitset.h
@@ -0,0 +1,11 @@
+#ifndef PRIME_BITSET_H
+#define PRIME_BITSET_H
+
+#include <vector>
+
+const std::vector<bool> PRIME_BITSET {
+0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 
+1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0
+};
+
+#endif
